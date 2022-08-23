1683117d8644d56dea3479cc27d3078a41ef81d6
JonyUcles <jony.heaply@gmail.com>
2022-08-22 21:45:55 -0700

    config jest

--- src/fetchContentful/fetchContentful.test.js
+++ src/fetchContentful/fetchContentful.test.js
@@ -3,3 +3,7 @@ test('Grab Contentful Data Success',()=>{
     //expect(fetchContentful()).toBe({});
     //planning out test
 })
+test('Grab Contentful Data Fail',()=>{
+    //expect(fetchContentful()).toBe({});
+    //planning out test
+})
